8fc43408823c86e7622cdc6f4ea575b7c95db74b
Sophia Almeida <sophia.almeida.work@gmail.com>
2026-02-01 19:06:27 -0300

    não sei oq estou fazendo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Configuração para GitHub Pages
 export default defineConfig({
-  base: '/Sistema-de-Produtos/',  // <--- nome do seu repositório
   plugins: [react()],
 })
 
